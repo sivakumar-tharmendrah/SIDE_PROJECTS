36bb7b1b835ba2db92cb7d915f7956811cb3df7f
sivakumar-tharmendrah <tharsiv@my.yorku.ca>
2023-01-12 18:42:16 -0500

    Computer toString() method
    toString() method for Vaccine class where it returns the description of each vaccine object

--- Vaccination Tracker/src/junit_tests/junit_test1.java
+++ Vaccination Tracker/src/junit_tests/junit_test1.java
@@ -4,9 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import model.Vaccine;
+
 public class junit_test1 {
-
-
 	
 	@Test
 	public void test_vaccine_01() {
@@ -23,6 +23,21 @@ public class junit_test1 {
 		assertEquals("Recognized vaccine: BNT162b2 (RNA; Pfizer/BioNTech)", v2.toString());
 		assertEquals("Recognized vaccine: Ad26.COV2.S (Non Replicating Viral Vector; Janssen)", v3.toString());
 		assertEquals("Recognized vaccine: AZD1222 (Non Replicating Viral Vector; Oxford/AstraZeneca)", v4.toString());
+	}
+	
+	@Test
+	public void test_vaccine_02() {
+		/* 
+		 * Initialize records of vaccines not recognized in Canada. 
+		 * Input values of the constructor consist of the codename, type, and manufacturer. 
+		 */ 
+		Vaccine v5 = new Vaccine("Covishield", "Non Replicating Viral Vector", "Serum Institute of India");
+		Vaccine v6 = new Vaccine("BBIBP-CorV", "Inactivated", "Sinopharm");
+		Vaccine v7 = new Vaccine("CoronaVac", "Inactivated", "Sinovac"); 
+
+		assertEquals("Unrecognized vaccine: Covishield (Non Replicating Viral Vector; Serum Institute of India)", v5.toString());
+		assertEquals("Unrecognized vaccine: BBIBP-CorV (Inactivated; Sinopharm)", v6.toString());
+		assertEquals("Unrecognized vaccine: CoronaVac (Inactivated; Sinovac)", v7.toString()); 
 	}
 
 }
--- Vaccination Tracker/src/model/Vaccine.java
+++ Vaccination Tracker/src/model/Vaccine.java
@@ -1,5 +1,26 @@
 package model;
 
 public class Vaccine {
+	
+	private String status;
+	private String codename;
+	private String type;
+	private String manufacturer;
+	
+	public Vaccine(String codename, String type, String manufacturer) {
+		this.codename = codename;
+		this.type = type;
+		this.manufacturer = manufacturer;
+		this.status = String.format("Recognized vaccine: %s (%s; %s)", this.codename, this.type, this.manufacturer);
+		
+	}
+	
+	public String toString() {
+		return this.status;
+	}
+	
+	
+	
+	
 
 }
