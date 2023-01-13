63293cf960f3cbbb4ceb848c0c7e19bc8217d506
sivakumar-tharmendrah <tharsiv@my.yorku.ca>
2023-01-13 11:25:48 -0500

    Added Canadian recognized status
    If a vaccine is recognized by the canadian government, it will be classified as "recognized vaccine"

--- Vaccination Tracker/src/model/Vaccine.java
+++ Vaccination Tracker/src/model/Vaccine.java
@@ -10,17 +10,24 @@ public class Vaccine {
 	public Vaccine(String codename, String type, String manufacturer) {
 		this.codename = codename;
 		this.type = type;
-		this.manufacturer = manufacturer;
-		this.status = String.format("Recognized vaccine: %s (%s; %s)", this.codename, this.type, this.manufacturer);
+		this.manufacturer = manufacturer; 
+		//mRNA-1273, BNT162b2, Ad26.COV2.S, and AZD1222
+		if(		this.codename.equals("mRNA-1273")
+			||  this.codename.equals("BNT162b2")
+			||  this.codename.equals("Ad26.COV2.S")
+			||  this.codename.equals("AZD1222")
+		) {
+			this.status = String.format("Recognized vaccine: %s (%s; %s)", this.codename, this.type, this.manufacturer);
+		}
+		else {
+			this.status = String.format("Unrecognized vaccine: %s (%s; %s)", this.codename, this.type, this.manufacturer);
+
+		}
 		
 	}
 	
 	public String toString() {
 		return this.status;
 	}
-	
-	
-	
-	
 
 }
