5d2260e08040c0ad725e8853df5bfa1cea37806d
sivakumar-tharmendrah <tharsiv@my.yorku.ca>
2023-02-28 19:56:18 -0500

    Added additional tests

--- Building Design/src/model/Blueprint.java
+++ Building Design/src/model/Blueprint.java
@@ -1,10 +1,13 @@
 package model;
 
+
 public class Blueprint {
 	private Floor[] floors;
 	private int nof; /* number of floors */
+	private int maxNumOfFloors;
 
 	public Blueprint(int maxNumberOfFloors) {
+		this.maxNumOfFloors = maxNumberOfFloors;
 		this.floors = new Floor[maxNumberOfFloors];
 		this.nof = 0;
 	}    
@@ -18,7 +21,7 @@ public class Blueprint {
 		Floor[] results = new Floor[this.nof];
 		
 		for(int i = 0; i < this.nof; i ++) {
-			results[i] = this.floors[i];
+			results[i] = new Floor(this.floors[i]);
 		}
 		
 		return results;
@@ -26,5 +29,13 @@ public class Blueprint {
 	
 	public String toString() {
 		//"0.0 percents of building blueprint completed (0 out of 7 floors)"
+		
+		double percentage = (this.nof / (double) this.maxNumOfFloors) * 100;
+		String percentageS = String.format("%.1f", percentage);
+		
+		return String.format("%s percents of building blueprint completed (%d out of %d floors)", 
+				percentageS, this.nof, this.maxNumOfFloors);
+	
+		 
 	}
 }
