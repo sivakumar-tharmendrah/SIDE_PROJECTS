dd978066e9b28c573add410a2ebff643a55be6dd
sivakumar-tharmendrah <tharsiv@my.yorku.ca>
2023-03-20 11:22:38 -0400

    added more code

--- Apple AppStore/src/model/App.java
+++ Apple AppStore/src/model/App.java
@@ -1,16 +1,27 @@
 package model;
 
 public class App {
+	private Log[] updates;
+	private int nou; /* number of updates */
+	private final int MAX_NUM_OF_UPDATES = 20;
+	
 	private String name;
 	
 	public App(String name, int maxNumOfRatings) {
+		this.name = name;
+		this.updates = new Log[MAX_NUM_OF_UPDATES];
+		this.nou = 0;
+	}
+	
+	public void releaseUpdate(String version) {
 		
 	}
 	
 	public String getName() {
-		return null;
+		return this.name;
 	}
 	
+	//return info for latest version (last element in array for log)
 	public String getWhatIsNew() {
 		return "n/a";
 	}
@@ -19,6 +30,7 @@ public class App {
 		return new Log[0];
 	}
 	
+	
 	public Log getVersionInfo(String version) {
 		return null;
 	}
@@ -26,5 +38,11 @@ public class App {
 	public String getRatingReport() {
 		return "No rating submitted so far";
 	}
+	
+	public String toString() {
+		return String.format("%s (Current Version: %s; Average Rating: n/a)", 
+					this.name, this.getWhatIsNew());
+	}
 
+	
 }
